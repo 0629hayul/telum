adccd9b78ee03340c28840383e0d59b81764b966
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-05-21 22:11:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -44,7 +44,10 @@ async def on_message(message):
             # 이미 메시지를 보낸 경우
             await message.delete()
             # 제한 시간 계산
-            remaining_time = (datetime.datetime.now() + cooldown_time) - datetime.datetime.now()
+            remaining_time_seconds = (datetime.datetime.now() + cooldown_time) - datetime.datetime.now()
+            remaining_time_hours = int(remaining_time_seconds.total_seconds() // 3600)
+            remaining_time_minutes = int((remaining_time_seconds.total_seconds() % 3600) // 60)
+            remaining_time = f"{remaining_time_hours}시간 {remaining_time_minutes}분"
             # DM으로 알림 전송
             dm_message = f"1일 1회 제한 (남은시간: {remaining_time})"
             await message.author.send(dm_message)
