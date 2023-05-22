bfab2b376210019c0253afe22482a2d0d0386909
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-05-22 20:20:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -44,7 +44,7 @@ async def on_message(message):
 async def decrease_cooldown(author_id):
     while cooldowns.get(author_id, 0) > 0:
         cooldowns[author_id] -= 1
-        await time.sleep(1)
+        time.sleep(1)
     if author_id in cooldowns:
         del cooldowns[author_id]
 
