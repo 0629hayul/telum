2e94defce96cbce13423db52c61e9a5e4145169e
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-05-22 21:39:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,24 +1,21 @@
-import discord
-from discord.ext import commands
-import asyncio
 from cmath import log
 from distutils.sysconfig import PREFIX
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import os
-import time
+import asyncio
+from datetime import datetime, timedelta
 import datetime
 
 load_dotenv()
 
-intents = discord.Intents.default()
-intents.typing = False
-intents.presences = False
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
-client = discord.Client(intents=intents)
-cooldowns = {}
+msg_ed = {}
+channel_id = 1109828429261590669
+
+client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -29,19 +26,27 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.channel.id == 1109828429261590669:  # 특정 체널 ID
-        author_id = message.author.id
+    user_id = message.author.id
+    now = datetime.now()
 
-        if author_id not in cooldowns or cooldowns[author_id] <= datetime.now():
-            cooldowns[author_id] = datetime.now() + timedelta(seconds=30)
-            await process_commands(message)
-        else:
-            await message.delete()
-            cooldown_remaining = cooldowns[author_id] - datetime.now()
-            await message.author.send(f"You are on cooldown. Please wait for {cooldown_remaining.total_seconds():.0f} seconds.")
+    if user_id in msg_ed and msg_ed[user_id] == 1:
+        await message.delete()
+        return
 
-
-
+    msg_ed[user_id] = 1
+    await client.process_commands(message)
+    
+   
+# 매일 오전 1시에 유저별 메시지 보낸 여부 초기화
+async def reset_msg_ed():
+    await client.wait_until_ready()
+    while not client.is_closed():
+        now = datetime.now()
+        if now.hour == 1 and now.minute == 0:
+            msg_ed.clear()
+        await asyncio.sleep(60)  # 60초(1분)마다 반복
+        
+        
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:
