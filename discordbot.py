b75f48d46698f221925701ca3c7386803a9e6872
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-05-22 21:08:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -32,37 +32,14 @@ async def on_message(message):
     if message.channel.id == 1109828429261590669:  # 특정 체널 ID
         author_id = message.author.id
 
-        if author_id not in cooldowns or cooldowns[author_id] == 0:
-            cooldowns[author_id] = 30
-            await decrease_cooldown(author_id)
+        if author_id not in cooldowns or cooldowns[author_id] <= datetime.now():
+            cooldowns[author_id] = datetime.now() + timedelta(seconds=30)
             await process_commands(message)
         else:
             await message.delete()
-            await message.author.send(f"You are on cooldown. Please wait for {cooldowns[author_id]} seconds.")
+            cooldown_remaining = cooldowns[author_id] - datetime.now()
+            await message.author.send(f"You are on cooldown. Please wait for {cooldown_remaining.total_seconds():.0f} seconds.")
 
-
-async def decrease_cooldown(author_id):
-    while cooldowns.get(author_id, 0) > 0:
-        cooldowns[author_id] -= 1
-        time.sleep(1)
-    if author_id in cooldowns:
-        del cooldowns[author_id]
-
-async def process_commands(message):
-    if not message.content.startswith('!'):
-        return
-
-    ctx = await client.get_context(message)
-    if ctx.command is None:
-        return
-
-    try:
-        await client.invoke(ctx)
-    except commands.CommandError as error:
-        if isinstance(error, commands.CommandOnCooldown):
-            cooldown = error.retry_after
-            cooldown_seconds = round(cooldown)
-            await ctx.author.send(f"You are on cooldown. Please wait for {cooldown_seconds} seconds.")
 
 
 try:
