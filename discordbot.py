23adb51a34e2101cf8a11a02534e862218591fa2
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-05-22 17:55:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,7 +11,7 @@ load_dotenv()
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 cooldown_dict = {}
-cooldown_time = datetime.timedelta(seconds=86400)
+#cooldown_time = datetime.timedelta(seconds=86400)
 
 client = discord.Client()
 
@@ -29,27 +29,19 @@ async def on_message(message):
 
     if message.content.startswith(f'{PREFIX}hello'):
         await message.channel.send('Hello!')
-    if message.channel.id == 1109828429261590669:
-        author_id = str(message.author.id) 
-        today = datetime.datetime.now().date() #
+    cooldown_time = timedelta(hours=24)  # 쿨다운 시간
 
-        # 사용자가 처음 메시지를 보내는 경우 또는 제한 시간이 지난 경우
-        if author_id not in cooldown_dict or cooldown_dict[author_id] < today:
-            cooldown_dict[author_id] = 86400
-            # 관리자가 확인할 수 있도록 원하는 작업을 수행하고, 이 부분은 알맞게 수정해야 합니다.
-            # 여기서는 관리자가 해당 메시지를 확인한다고 가정하고, 콘솔에 출력합니다.
-            print(f"New message: {message.content}")
-        else:
-            # 이미 메시지를 보낸 경우
+    if message.channel.id == channel_id:
+        author_id = str(message.author.id)
+        now = datetime.now()
+
+        if author_id in cooldown_dict and cooldown_dict[author_id] > now:
             await message.delete()
-            # 제한 시간 계산
-            remaining_time_seconds = (datetime.datetime.combine(cooldown_dict[author_id], datetime.time.max) + cooldown_time) - datetime.datetime.now()
-            remaining_time_hours = int(remaining_time_seconds.total_seconds() // 3600)
-            remaining_time_minutes = int((remaining_time_seconds.total_seconds() % 3600) // 60)
-            remaining_time = f"{remaining_time_hours}시간 {remaining_time_minutes}분"
-            # DM으로 알림 전송
-            dm_message = f"1일 1회 제한 (남은시간: {remaining_time})"
-            await message.author.send(dm_message)
+            return
+
+        cooldown_dict[author_id] = now + cooldown_time
+
+    await client.process_commands(message)
 
 try:
     client.run(TOKEN)
